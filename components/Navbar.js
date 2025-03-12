055e9528c5b4a2f746e3aa2bc8049cc25b1a0fc4
Evan Perry <eperry2688@gmail.com>
2025-03-12 13:14:37 -0600

    Navbar reverted to be more inline with original react design

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,83 +1,60 @@
 "use client";
-import { useState } from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./Navbar.css";
 
-export default function SiteNavbar() {
+import React, { useState } from "react";
+import { Navbar, Nav, NavLink, Container } from "react-bootstrap";
+import Link from "next/link";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "./NavBar.css";
+
+const NavBar = () => {
   const [expanded, setExpanded] = useState(false);
-  const pathname = usePathname();
+
+  const handleToggle = () => setExpanded(!expanded);
+  const handleLinkClick = () => setExpanded(false);
 
   const navLinks = [
-    {
-      to: "/",
-      label: "Home",
-    },
-    {
-      to: "/services",
-      label: "Services",
-    },
-    {
-      to: "/about",
-      label: "About",
-    },
-    {
-      to: "/partnership",
-      label: "Partnership",
-    },
-    {
-      to: "/research",
-      label: "Research",
-    },
-    {
-      to: "/careers",
-      label: "Careers",
-    },
-    {
-      to: "/contact",
-      label: "Contact",
-    },
+    { to: "/", label: "Home" },
+    { to: "/services", label: "Services" },
+    { to: "/about", label: "About" },
+    { to: "/partnership", label: "Partnership" },
+    { to: "/research", label: "Research" },
+    { to: "/careers", label: "Careers" },
+    { to: "/contact", label: "Contact" },
   ];
 
   return (
-    <div>
+    <div className="w-100">
       <Navbar
         fixed="top"
         bg="black"
         variant="black"
         expand="lg"
         expanded={expanded}
-        onToggle={() => setExpanded(!expanded)}
+        onToggle={handleToggle}
       >
         <Container className="d-flex justify-content-between py-1">
-          <Navbar.Brand>
-            <Link href="/" className="text-light">
-              PROJXON
-            </Link>
+          <Navbar.Brand as={NavLink} to="/" className="text-light">
+            PROJXON
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Container className="navbar-container d-flex justify-content-md-start justify-content-lg-end">
               <Nav className="ml-auto text-uppercase">
-                {navLinks.map((link, index) => {
-                  const currPage =
-                    pathname === link.to ||
-                    (pathname.startsWith(link.to) && link.to != "/");
-
-                  return (
-                    <Nav.Link
-                      className={`link-offset-3${currPage ? " active" : ""}`}
-                      key={index}
-                      as="div"
+                {navLinks.map((link, index) => (
+                  <NavLink
+                    className="link-offset-3"
+                    key={index}
+                    as="div" // Render as a div since Next.js Link uses an anchor tag
+                  >
+                    <Link
+                      href={link.to}
+                      className="text-light"
+                      onClick={handleLinkClick}
                     >
-                      <Link href={link.to} onClick={() => setExpanded(false)}>
-                        {link.label}
-                      </Link>
-                    </Nav.Link>
-                  );
-                })}
+                      {link.label}
+                    </Link>
+                  </NavLink>
+                ))}
               </Nav>
             </Container>
           </Navbar.Collapse>
@@ -85,4 +62,6 @@ export default function SiteNavbar() {
       </Navbar>
     </div>
   );
-}
+};
+
+export default NavBar;
