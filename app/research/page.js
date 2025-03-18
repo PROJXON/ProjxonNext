d93680e3e3c42c5bb3e62fedd1d061d58ecaff56
davenpos <davenpos@uwindsor.ca>
2025-03-18 17:01:49 -0400

    Fixed spinner not loading when blog posts are loading

--- app/research/page.js
+++ app/research/page.js
@@ -1,25 +1,21 @@
 import Hero from "@/components/Hero";
 import CallToAction from "@/components/CallToAction";
 import BlogClientList from "@/components/BlogClientList";
-import { fetchBlogs } from "@/services/blogService";
-
 
 import "./ResearchPage.css";
 import AOSWrapper from "@/components/AOSWrapper";
 
 export default async function ResearchPage() {
-  const blogs = await fetchBlogs();
-
   return (
     <div>
-        <AOSWrapper />
-        <Hero
-          title="Blog & Research"
-          subtitle="Stay Updated with the Latest News, Events, and Insight"
-          backgroundClass="research-hero"
-        />
-        <BlogClientList initialBlogs={blogs} />
-        <CallToAction />
+      <AOSWrapper />
+      <Hero
+        title="Blog & Research"
+        subtitle="Stay Updated with the Latest News, Events, and Insight"
+        backgroundClass="research-hero"
+      />
+      <BlogClientList />
+      <CallToAction />
     </div>
   );
 }
