6cbea8d4f7d63f8ac69ac93920436f21da93fbe7
davenpos <davenpos@uwindsor.ca>
2025-07-01 18:40:43 -0400

    Removed unused React import on Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Navbar, Nav, NavLink, Container } from "react-bootstrap";
 import Link from "next/link";
 import "./NavBar.css";
