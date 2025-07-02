26be34f726e4c08603437795d90236746a2b862e
Evan Perry <eperry2688@gmail.com>
2025-07-02 14:28:23 -0600

    Add Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    'semi': ['error', 'always'],
+    // Add or override rules as needed
+  },
+}; 
