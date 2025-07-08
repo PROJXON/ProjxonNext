ca0d579c6e5dab86b73e8b4b7653bf762f107ad6
Brent Chen <1217brent@Brents-MacBook-Air.local>
2025-07-08 19:39:07 +0900

    Quickly fixed lint errors for comma & single quotes

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,5 +1,5 @@
-import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
   const { username, password }: { username: string; password: string } =
@@ -9,26 +9,26 @@ export async function POST(req: NextRequest) {
     const res = await fetch(
       `${process.env.WORDPRESS_JWT_URL}/jwt-auth/v1/token`,
       {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
-      }
+      },
     );
 
     if (!res.ok)
       return NextResponse.json(
-        { message: "Invalid credentials" },
-        { status: 401 }
+        { message: 'Invalid credentials' },
+        { status: 401 },
       );
 
     return Response.json(await res.json());
   } catch (error: unknown) {
     return new NextResponse(
       JSON.stringify({
-        message: "Authentication failed",
-        error: error instanceof Error ? error.message : "Unknown error",
+        message: 'Authentication failed',
+        error: error instanceof Error ? error.message : 'Unknown error',
       }),
-      { status: 500 }
+      { status: 500 },
     );
   }
 }
--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,31 +1,31 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import axios from "axios";
-import FormData from "form-data";
-import getAuth from "@/lib/getAuth";
-import { WPImage, UploadResponse } from "@/types/interfaces";
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import axios from 'axios';
+import FormData from 'form-data';
+import getAuth from '@/lib/getAuth';
+import { WPImage, UploadResponse } from '@/types/interfaces';
 
 export async function POST(req: NextRequest) {
   try {
     const authHeader = getAuth(req);
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
     }
-    const token = authHeader.split(" ")[1];
+    const token = authHeader.split(' ')[1];
 
     const formData = new FormData();
     const body = await req.formData();
-    const file = body.get("file");
+    const file = body.get('file');
     if (!file || !(file instanceof File)) {
       return NextResponse.json(
-        { message: "No file uploaded" },
-        { status: 400 }
+        { message: 'No file uploaded' },
+        { status: 400 },
       );
     }
 
     const buffer = Buffer.from(await file.arrayBuffer());
-    formData.append("file", buffer, file.name);
-    console.log("FormData:", formData);
+    formData.append('file', buffer, file.name);
+    console.log('FormData:', formData);
 
     // Make the POST request to the WordPress media API
     const response = await axios.post<WPImage>(
@@ -36,9 +36,9 @@ export async function POST(req: NextRequest) {
           ...formData.getHeaders(),
           Authorization: `Bearer ${token}`,
         },
-      }
+      },
     );
-    console.log("WordPress API response:", response.data);
+    console.log('WordPress API response:', response.data);
 
     // Return the image URL from the WordPress API
     const sourceURL: UploadResponse = { url: response.data.source_url };
@@ -46,12 +46,12 @@ export async function POST(req: NextRequest) {
   } catch (error: unknown) {
     const err = error as { response?: { data?: unknown } };
     console.error(
-      "Error uploading image:",
-      err.response ? err.response.data : error
+      'Error uploading image:',
+      err.response ? err.response.data : error,
     );
     return NextResponse.json(
-      { message: "Error uploading image" },
-      { status: 500 }
+      { message: 'Error uploading image' },
+      { status: 500 },
     );
   }
 }
