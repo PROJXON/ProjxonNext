0b15ca38eec7abeb316dc931150a580739ef056c
Evan Perry <eperry2688@gmail.com>
2025-08-25 14:02:47 -0600

    Lint fixes

--- components/ApplyNow.tsx
+++ components/ApplyNow.tsx
@@ -1,4 +1,4 @@
-import { JSX } from "react";
+import { JSX } from 'react';
 
 export default function ApplyNow(): JSX.Element {
   return (
@@ -7,28 +7,28 @@ export default function ApplyNow(): JSX.Element {
       style={{
         backgroundImage:
           "linear-gradient(to bottom, rgba(0,0,0,0.7), rgba(0,0,0,0.7)), url('/assets/internships/internship-background.webp')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        borderRadius: 0
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        borderRadius: 0,
     }}
     >
       <div style={{ margin: '3rem', width: '100%', maxWidth: '900px' }}>
         <h2
           className="text-yellow fw-bold mb-3 text-center"
-          style={{ fontSize: "2.3rem", marginBottom: "2rem" }}
+          style={{ fontSize: '2.3rem', marginBottom: '2rem' }}
         >
           Apply To Our Momentum Internship Program!
         </h2>
         <p
           className="text-gray mb-4 text-center"
           style={{
-            fontSize: "1.3rem",
-            marginBottom: "2.5rem",
-            maxWidth: "800px",
-            marginLeft: "auto",
-            marginRight: "auto",
-            textAlign: "center"
+            fontSize: '1.3rem',
+            marginBottom: '2.5rem',
+            maxWidth: '800px',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            textAlign: 'center',
           }}
         >
           Looking for professional and career development? Apply to our internship
@@ -41,11 +41,11 @@ export default function ApplyNow(): JSX.Element {
           rel="noopener noreferrer"
           className="text-yellow fw-bold text-decoration-underline text-center"
           style={{
-            fontSize: "1.3rem",
-            background: "none",
-            border: "none",
+            fontSize: '1.3rem',
+            background: 'none',
+            border: 'none',
             padding: 0,
-            display: "inline-block",
+            display: 'inline-block',
           }}
         >
           Apply Now
